54ab4c29e3cb6d318841c87c48ca2deb111d91a6
zkdlu <zkdlu159951@gmail.com>
2020-05-15 21:41:55 +0900

    add Disk Read Write View

---
+++ SystemMonitoring/ViewModels/DiskReadWriteViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace SystemMonitoring.ViewModels
+{
+    public class DiskReadWriteViewModel : BaseViewModel
+    {
+        private PerformanceCounter _diskReadsCounter;
+        private PerformanceCounter _diskWritesCounter;
+
+        private double _diskReadBytes;
+        public double DiskReadBytes
+        {
+            get { return _diskReadBytes; }
+            set
+            {
+                _diskReadBytes = value;
+                OnRaiseProperty(nameof(DiskReadBytes));
+            }
+        }
+
+        private double _diskWriteBytes;
+        public double DiskWriteBytes
+        {
+            get { return _diskWriteBytes; }
+            set
+            {
+                _diskWriteBytes = value;
+                OnRaiseProperty(nameof(DiskWriteBytes));
+            }
+        }
+
+        public DiskReadWriteViewModel()
+        {
+            _diskReadsCounter = new PerformanceCounter("PhysicalDisk", "Disk Read Bytes/sec", "_Total");
+            _diskWritesCounter = new PerformanceCounter("PhysicalDisk", "Disk Write Bytes/sec", "_Total");
+
+            StartDispatcher(UpdateReadWrites);
+        }
+
+        private void UpdateReadWrites()
+        {
+            DiskReadBytes = _diskReadsCounter.NextValue();
+            DiskWriteBytes = _diskWritesCounter.NextValue();
+        }
+    }
+}
